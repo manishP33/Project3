75f064d515badf674be0ba5291af3956c62823d9
RYANBROPHY <BROPHY2321@ME.COM>
2016-10-31 14:49:52 -0400

    comments

--- NDA/app/models/disaster.rb
+++ NDA/app/models/disaster.rb
@@ -1,3 +1,4 @@
 class Disaster < ApplicationRecord
   belongs_to :natural_disaster_category
+  has_many :comments
 end
--- NDA/config/routes.rb
+++ NDA/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :disasters
   resources :natural_disaster_categories
+  resources :disasters
+  resources :comments
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: redirect('/natural_disaster_categories')
 end
